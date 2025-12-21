4b7bdb279d52fc7b252e8dd041a81d0350361573
Zakharko Adrian <zakharkoadr@gmail.com>
2025-12-22 00:12:48 +0200

    Start js, variables + math operation + enter

---
+++ Js_learning/script.js
@@ -0,0 +1,65 @@
+// вивід
+document.write("Hello World"); // застаріле
+document.write("!!!");
+
+console.log("Hello Java script (print)");
+console.error("error");
+console.warm("warn");
+console.info("info"); //info=log
+
+
+//змінні
+
+var var_num; // var = old
+
+numik = 5;
+
+console.log(var_num);
+
+let let_num = 10;
+console.log(let_num); // переприсвоєння
+let_num = 3;
+console.log(let_num);
+
+console("Result: " + let_num + "!");
+
+const number = 10;
+
+console.log(number);
+
+let ff = 0.032; // фактично любі значення приймає не так як у c++
+let fff = "Adrian";
+
+let IsHisCar = true; // булеани
+
+// Math operation
+
+let num1 = 14;
+let num2 = 7;
+
+// first method
+let res = num1 + num2;
+console.log("Result: " + res);
+
+// second method
+console.log("Result" + (num1 + num2));
+console.log("Result" + (num1 - num2));
+console.log("Result" + (num1 * num2));
+console.log("Result" + (num1 / num2));
+console.log("Result" + (num1 % num2)); // залишок від ділення
+
+let num3 = 5; //переведеться у текст, бо друга змінна яка була придодавані текст
+let num4 = "10";
+
+let qqq = Number("12"); // переведе у число
+console.log(num3 + Number(qqq)); // тут також можна
+
+console.log(num3 + num4);
+
+num5 += +5; // + - * /
+numm++; //++ --
+
+let pi = Math.pi;
+
+console.log(Math.pi); //Math.sin Math.cos Math.abs Math.min Math.max
+console.log(Math.max(-1, 2, 10, 5));
