a5df9dcfee0b2a30efbec67680289d406fa2aa9c
Zakharko Adrian <zakharkoadr@gmail.com>
2025-12-28 01:37:31 +0200

    Події та обробник подій у JavaScript

--- Js_learning/script.js
+++ Js_learning/script.js
@@ -306,3 +306,30 @@ else console.log(resArr2);
 // alert return = empty; confirm and prompt виводять
 
 
+// Події та обробник подій у JavaScript
+
+let counter_1 = 0;
+function Btnclick(el) {
+    counter_1++;
+    el.innerHTML = "Count: " + counter_1;
+
+    // alert("Message");
+    // el.name = "We can change the name also in HTML";
+    // console.log(el.name);
+}
+
+function BtnDblclick(){
+    console.log("Дебил");
+}
+
+function inputButton(el) {
+    if (el.value = "font"){
+        el.style.fontSize = "20px";
+    }
+    else if (el.value = "color"){
+        el.style.color = "red";
+    }
+    else if (el.value = "bg"){
+        el.style.backgroundColor = "black";
+    }
+}
