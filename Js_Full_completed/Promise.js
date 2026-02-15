6b34c7608193713b12e99e3b24f004b5d804645d
Zakharko Adrian <zakharkoadr@gmail.com>
2026-02-16 01:11:01 +0200

    JS-9.1

--- Js_Full_completed/Promise.js
+++ Js_Full_completed/Promise.js
@@ -36,3 +36,44 @@ promise
     })
     .catch(error => console.log(error))
     .finally(() => alert('The job is done!'));
+
+
+
+
+
+
+
+
+function getPostsById(id) {
+    const endpoint = 'https://jsonplaceholder.typicode.com';
+    const result = {
+        id,
+        name: '',
+        posts: []
+    }
+
+    return new Promise((resolve, reject) => {
+        // 1. Запит за юзерами
+        fetch(`${endpoint}/users`)
+            .then(data => data.json())
+            .then(users => {
+                // 2. Знайшли юзера
+                const foundUser = users.find(user => user.id === id);
+                result.name = foundUser.name;
+
+                // 3. Запит за постами (Всередині першого!)
+                fetch(`${endpoint}/posts`)
+                    .then(data => data.json())
+                    .then(posts => {
+                        // 4. Відфільтрували пости цього юзера
+                        result.posts = posts.filter(post => post.userId === id);
+                        
+                        // 5. Повернули готовий об'єкт
+                        resolve(result);
+                    });
+            });
+    });
+}
+
+getPostsById(1).then(data => console.log(data));
+console.log(11111);
